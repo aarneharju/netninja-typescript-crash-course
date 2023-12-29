28f92f9797b911eeddbdc16a6336a8b72193b942
aarneharju <35223549+aarneharju@users.noreply.github.com>
2023-12-29 21:22:47 +0200

    type basics done

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-let age = 30;
+let age: number = 30;
+let firstName: string = 'Mario';
+let isFictional: boolean;
 
-console.log(age);
+age = 31;
+firstName = 'Luigi';
+isFictional = false;
+
+// Typescript infers type automatically, can't assign them different values
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+
+// null & undefined also can't be changed after assigned
+let something: null;
+let anotherThing: undefined;
