10dc45fbd5c72a8bc7301c6197fb14279fba70ce
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-01-07 14:33:28 +0200

    arrays and object literals done

--- dist/index.js
+++ dist/index.js
@@ -13,3 +13,28 @@ let isLarge = false;
 let something;
 let anotherThing;
 console.log(firstName);
+// Arrays
+let names = ['Mario', 'Luigi', 'Gaba'];
+let ages = [30, 31, 29];
+let bools = [true, false, true];
+// Type inference with arrays
+let fruits = ['apples', 'pears', 'bananas', 'mangos'];
+fruits.push('grapes');
+const f = fruits[3];
+let things = [1, true, 'hello'];
+const t = things[0]; // not necessary always a number
+//----------------
+// Object literals
+//----------------
+// Tuples
+let address = ['123 Main St', 98076];
+// Enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let myColor = Color.Green;
+// Any
+let car = 'BMW';
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ age = 31;
 firstName = 'Luigi';
 isFictional = false;
 
-// Typescript infers type automatically, can't assign them different values
+// Typescript infers type automatically, can't assign them different types afterwards
 let planet = 'Earth';
 let moons = 1;
 let isLarge = false;
@@ -16,3 +16,58 @@ let something: null;
 let anotherThing: undefined;
 
 console.log(firstName);
+
+//-------
+// Arrays
+//-------
+
+let names: string[] = ['Mario', 'Luigi', 'Gaba'];
+let ages: number[] = [30, 31, 29];
+let bools: boolean[] = [true, false, true];
+
+// Type inference with arrays
+
+let fruits = ['apples', 'pears', 'bananas', 'mangos'];
+
+fruits.push('grapes');
+
+const f = fruits[3];
+
+let things = [1, true, 'hello'];
+
+const t = things[0]; // not necessary always a number
+
+//----------------
+// Object literals
+//----------------
+
+let user: { firstName: string; age: number; id: number } = {
+  firstName: 'Mario',
+  age: 30,
+  id: 123,
+};
+
+// Type inference with object literals
+
+let person = {
+  name: 'luigi',
+  score: 235,
+};
+
+// Tuples
+
+let address: [string, number] = ['123 Main St', 98076];
+
+// Enums
+
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+
+let myColor: Color = Color.Green;
+
+// Any
+
+let car: any = 'BMW';
