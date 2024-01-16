cab8a3ac1b43b6efee514984c6e304f8dd8c6374
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-01-16 21:44:17 +0200

    type aliases done

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
-let age = 30;
+let age1 = 30;
 let firstName = 'Mario';
 let isFictional;
-age = 31;
+age1 = 31;
 firstName = 'Luigi';
 isFictional = false;
-// Typescript infers type automatically, can't assign them different values
+// Typescript infers type automatically, can't assign them different types afterwards
 let planet = 'Earth';
 let moons = 1;
 let isLarge = false;
@@ -13,7 +13,9 @@ let isLarge = false;
 let something;
 let anotherThing;
 console.log(firstName);
+//-------
 // Arrays
+//-------
 let names = ['Mario', 'Luigi', 'Gaba'];
 let ages = [30, 31, 29];
 let bools = [true, false, true];
@@ -21,14 +23,69 @@ let bools = [true, false, true];
 let fruits = ['apples', 'pears', 'bananas', 'mangos'];
 fruits.push('grapes');
 const f = fruits[3];
-let things = [1, true, 'hello'];
-const t = things[0]; // not necessary always a number
+let things1 = [1, true, 'hello'];
+const t = things1[0]; // not necessary always a number
 //----------------
 // Object literals
 //----------------
+let user = {
+    firstName: 'Mario',
+    age: 30,
+    id: 123,
+};
+// Type inference with object literals
+let person = {
+    name: 'luigi',
+    score: 235,
+};
+//---------
+// Functions
+//---------
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const subtractTwoNumbers = (a, b) => {
+    return a - b;
+};
+// When nothing returned, the function return type is void. Which will be inferred, if not set manually
+function addAllNumbers(items) {
+    const total = items.reduce((a, c) => a + c, 0);
+    console.log(total);
+}
+// Return type inference
+function formatGreeting(name, greeting) {
+    return `${greeting}, ${name}`;
+}
+//---------
+// Any type
+//---------
+let age;
+age = 30;
+age = false; // Basically turns typescript into normal javascript
+//-------------------
+// Any type in arrays
+//-------------------
+let things = ['hello', true, 123, null];
+things.push({ id: 234 });
+//-------
 // Tuples
-let address = ['123 Main St', 98076];
+//-------
+let address = ['123 Main St', 98076, true];
+let hsla = [200, '100%', '50%', 0.5];
+function useCoords() {
+    // get coords
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+//-------------
+// Named tuples
+//-------------
+let user1;
+user1 = ['Bob', 30];
+// -----
 // Enums
+// -----
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
@@ -36,5 +93,36 @@ var Color;
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
 let myColor = Color.Green;
-// Any
-let car = 'BMW';
+const authorOne = {
+    name: 'John Doe',
+    avatar: 'https://randomuser.me/api/portraits/men/1.jpg',
+};
+const newPost = {
+    title: 'My awesome post',
+    body: 'Lorem ipsum dolor sit amet',
+    tags: ['typescript', 'webdev'],
+    create_at: new Date(),
+    author: authorOne,
+};
+// Interfaces as function argument types
+function createPost(post) {
+    console.log(`Created post ${post.title} by ${post.author.name}`);
+}
+createPost(newPost);
+// Interfaces with arrays
+let posts = [newPost];
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b]; // return a tuple
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+const userOne = { name: 'mario', score: 75 };
+function formatUser(user) {
+    console.log(`${user.name}: ${user.score}`);
+}
+formatUser(userOne);
+formatUser({ name: 'luigi', score: 30 });
--- src/index.ts
+++ src/index.ts
@@ -171,3 +171,41 @@ function createPost(post: Post): void {
 createPost(newPost);
 
 // Interfaces with arrays
+
+let posts: Post[] = [newPost];
+
+//-------------
+// Type aliases
+//-------------
+
+// example 1 - tuple
+
+type RGB = [number, number, number];
+
+function getRandomColor(): RGB {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return [r, g, b]; // return a tuple
+}
+
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+
+// example 2 - object literal
+
+type User = {
+  name: string;
+  score: number;
+};
+
+const userOne: User = { name: 'mario', score: 75 };
+
+function formatUser(user: User): void {
+  console.log(`${user.name}: ${user.score}`);
+}
+
+formatUser(userOne);
+formatUser({ name: 'luigi', score: 30 });
